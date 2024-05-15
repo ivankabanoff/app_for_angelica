2d002e019daaa8a8b8121ceae0eb0b69387ff94d
Ivan Kabanov <kabanov_oi197@mail.ru>
2024-05-16 01:49:28 +0300

    Revert "structure changed"
    This reverts commit a884d48fc5318218a20337774f853789d2e3250d.

--- app/src/main/java/com/example/lognormalcalculator/LognormalCalculator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lognormalcalculator
-
-class LognormalCalculator {
-
-    internal fun generateRandomLognormalNumber(mean: Double, variance: Double): Double {
-        val randomNormalNumber = Math.random().let { it * 2.0 - 1.0 }
-        return Math.exp(mean + randomNormalNumber * variance / Math.sqrt(2.0))
-    }
-}
